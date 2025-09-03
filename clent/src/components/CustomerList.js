6ea0368e17c022ea904d6be99b69b21ead98fa09
wassemashaik <110347609+wassemashaik@users.noreply.github.com>
2025-09-03 11:04:24 +0530

    Update CustomerList.js

--- clent/src/components/CustomerList.js
+++ clent/src/components/CustomerList.js
@@ -30,7 +30,7 @@ const CustomerList = () => {
       queryParams.append("limit", limit);
 
       const response = await fetch(
-        `http://localhost:5000/api/customers?${queryParams.toString()}`
+        `${process.env.REACT_APP_API_URL}?${queryParams.toString()}`
       );
       console.log(response);
       if (!response.ok) throw new Error("Failed to fetch customers");
