f90d5b455ce0bea83a6aaf40df2c97477807b25c
wassemashaik <110347609+wassemashaik@users.noreply.github.com>
2025-09-03 11:04:54 +0530

    Update AddCustomerFormPage.js

--- clent/src/pages/AddCustomerFormPage.js
+++ clent/src/pages/AddCustomerFormPage.js
@@ -7,7 +7,7 @@ const AddCustomerFormPage = () => {
 
   const handleAdd = async (formData) => {
     try {
-      const response = await fetch("http://localhost:5000/api/customers", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/customers`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
